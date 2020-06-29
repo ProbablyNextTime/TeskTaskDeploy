d74dda4fccca7e99fb24a165449577e55eb79f78
Shya <rumarenko3@gmail.com>
2020-06-29 15:15:21 +0300

    deploy

---
+++ build/precache-manifest.778cab4daf1fd077d83606b405e175e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8d311ae579faf278a0f1718893f0365",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82807343b354b2582045",
+    "url": "/static/js/2.7b1d2b4e.chunk.js"
+  },
+  {
+    "revision": "bd24a75b0d6ff35dc82539e9b86cdbf4",
+    "url": "/static/js/2.7b1d2b4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62393117e28caf0bb68c",
+    "url": "/static/js/main.2a292034.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
