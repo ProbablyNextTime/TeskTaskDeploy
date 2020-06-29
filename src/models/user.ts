9bd9f3d6172e6d7aa7f54c3a3689274bb64d765f
Shya <rumarenko3@gmail.com>
2020-06-29 15:10:11 +0300

    first commit

---
+++ src/models/user.ts
@@ -0,0 +1,24 @@
+import mongoose, {Schema, Document, Types} from "mongoose";
+
+interface ISurveyAnswers  {
+  surveyId: mongoose.Types.ObjectId;
+  answers: string[]
+}
+
+export interface UserInterface extends Document{
+  _id: mongoose.Types.ObjectId;
+  username: string;
+  password: string;
+  role: string;
+  completedSurveys: ISurveyAnswers[]
+}
+
+const UserSchema: Schema = new Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  role: { type: String, required: true },
+  completedSurveys: [{ surveyId: mongoose.Types.ObjectId, answers: Array(String)}],
+});
+
+const User = mongoose.model<UserInterface>("User", UserSchema);
+export default User;
---
+++ src/server.ts
@@ -0,0 +1,100 @@
+import express, { Application, Request, Response } from "express";
+import bodyParser from "body-parser"
+import connect from "./DBConnection"
+import User from "./models/user";
+import {UserInterface} from "./models/user";
+import jwt from "jsonwebtoken"
+import { surveysRouter } from "./routes/survey"
+import bcrypt from "bcrypt"
+const dotenv = require('dotenv');
+const path = require("path")
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.PORT || 4000;
+
+// Needed to parse request body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Defining subRoute for surveys
+app.use("/api/surveys", surveysRouter);
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+// login user
+app.post("/api/login", async (req: Request, res: Response) => {
+  try {
+    const user: UserInterface | null = await User.findOne({username: req.body.user.username});
+    console.log(user, "dadaya")
+    if(user && bcrypt.compareSync(req.body.user.password, user.password)) {
+      // Sign jwt token
+      const token = jwt.sign({user : user}, process.env.SECRET_KEY || "SECRET");
+
+      if(token) {
+        // If token created send user and accessToken
+        res.status(200).send({user, accessToken: token});
+      } else {
+        // If token wasn't created send corresponding message
+        res.status(500).send({user: null, message: "Jwt setting failed"});
+      }
+    }
+    else {
+      // If users data doesn't match send corresponding message
+      res.status(400).send({user: null, message: "Incorrect login or password"})
+    }
+  } catch (err) {
+    console.log(err); // TODO: Error handling
+    res.status(500).send({message: "Unknown error"})
+  }
+});
+
+// SignUp newUser
+app.post("/api//signUp", async (req: Request, res: Response) => {
+  try {
+    const user: UserInterface | null = await User.findOne({username: req.body.user.username})
+
+    if(user) {
+      // If such user already exists send corresponding message
+      res.status(409).send({message: "such user exists"})
+    } else {
+      // Create new user
+      const newUser: UserInterface = new User({
+        username: req.body.user.username,
+        password: bcrypt.hashSync(req.body.user.password, 10),
+        role: "user",
+    })
+
+      await newUser.save()
+      res.status(200).send({user});
+    }
+  } catch (err) {
+    res.send(500).send({message: "Unknown server error"});
+  }
+})
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// default endpoint
+app.get("*", (req: Request, res: Response) => {
+  res.status(404).send("Not found");
+});
+
+// set headers for option requests
+app.options("/*", function(req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.send(200);
+});
+
+// connecting to DB
+connect( `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASSWORD}@supercluster10k.qsysn.mongodb.net/SUperCLuster10k?retryWrites=true&w=majority`);
+
+// starting server
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
+});
